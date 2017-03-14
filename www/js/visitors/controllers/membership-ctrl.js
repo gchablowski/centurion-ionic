53af7cbeb1f61463a38ed7eb3738ae9bc694f589
gerald Chablowski <g.chablowski@gmail.com>
2017-03-14 09:28:04 +0100

    Create membership page

--- www/js/visitors/controllers/membership-ctrl.js
+++ www/js/visitors/controllers/membership-ctrl.js
@@ -1,15 +1,8 @@
 'use strict';
 angular.module('visitors')
-        .controller('MembershipCtrl', ["$scope", "$window", "$location", "MainServ", function ($scope, $window, $location, MainServ) {
-                $scope.memberships = MainServ.memberships();
-
-                $scope.dropdownMore = function () {
-                    $('.read-more-info').toggleClass('show-info');
-                    $('.read-more').toggleClass('dissapear');
-                }
-
-                $scope.dropdown = function (event) {
-                    $(event.target).parent().siblings('.hidden-info').toggleClass('show-info');
-                }
+        .controller('MembershipCtrl', ["$scope", "MainServ", function ($scope, MainServ) {
+               
+                        $scope.memberships = MainServ.memberships();
+        
             }]);
         

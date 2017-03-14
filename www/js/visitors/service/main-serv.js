54d10d8cebe22a60debe36506b382d8ff3462cc4
gerald Chablowski <g.chablowski@gmail.com>
2017-03-14 16:57:33 +0100

    Create reciprocal page

--- www/js/visitors/service/main-serv.js
+++ www/js/visitors/service/main-serv.js
@@ -2,9 +2,10 @@
 angular.module('visitors')
         .factory('MainServ', ['Config', '$resource',
             function (Config, $resource) {
-                return $resource(Config.ENV.SERVER_URL + '/visitors/:action', {}, {
+                return $resource(Config.ENV.SERVER_URL + '/visitors/:action/:id', {id: '@id'}, {
                     memberships: {method: 'GET', params: {action: 'memberships'}, isArray: false},
                     listings: {method: 'GET', params: {action: 'listings'}, isArray: false},
                     reciprocals: {method: 'GET', params: {action: 'reciprocal'}, isArray: false},
+                    reciprocal: {method: 'GET', params: {action: 'listing'}, isArray: false}
                 });
             }]);
--- www/js/visitors/visitors.js
+++ www/js/visitors/visitors.js
@@ -21,15 +21,26 @@ angular.module('visitors', ['ionic', 'ngResource'])
                         controller: 'ListingsCtrl as ctrl',
                         resolve: {
                             MainServ: 'MainServ',
-                            datasets: function (MainServ) {
+                            datasets: ['MainServ', function (MainServ) {
                                 return MainServ.listings().$promise;
-                            }
+                            }]
                         }
                     })
                     .state('reciprocals', {
                         url: '/reciprocals',
                         templateUrl: 'templates/visitors/reciprocals-ctrl.html',
                         controller: 'ReciprocalsCtrl as ctrl'
+                    })
+                    .state('reciprocal', {
+                        url: '/reciprocal/:id',
+                        templateUrl: 'templates/visitors/reciprocal-tpl.html',
+                        controller: 'ListingCtrl  as ctrl',
+                        resolve: {
+                            MainServ: 'MainServ',
+                            datasets: ['MainServ', '$stateParams', function (MainServ, $stateParams) {
+                                return MainServ.reciprocal({id:$stateParams.id}).$promise;
+                            }]
+                        }
                     });
             // if none of the above states are matched, use this as the fallback
             $urlRouterProvider.otherwise('/visitors');

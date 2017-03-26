6edfea0db197c7407b72174c9b37c4339ea384d0
gerald Chablowski <g.chablowski@gmail.com>
2017-03-26 11:46:04 +0200

    add ionic modal on about page

--- www/js/visitors/controllers/about-ctrl.js
+++ www/js/visitors/controllers/about-ctrl.js
@@ -1,14 +1,28 @@
 'use strict';
 angular.module('visitors')
-        .controller('AboutCtrl', ['$scope', '$window', 'datasets', function ($scope, $window, datasets) {
-
+        .controller('AboutCtrl', ['$scope', '$window', 'datasets', '$ionicModal', function ($scope, $window, datasets, $ionicModal) {
+                $scope.modalcontent = '';
                 $scope.gallery = datasets.gallery;
                 $scope.video1 = datasets.video1;
                 $scope.video2 = datasets.video2;
 
-
                 $scope.launchMap = function () {
                     $window.open('maps:q=51.741901,-0.407283', '_system');
+                };
+
+                $ionicModal.fromTemplateUrl('templates/visitors/about-modal-ctrl.html', {
+                    scope: $scope,
+                    animation: 'zoom-in'
+                }).then(function (modal) {
+                    $scope.modal = modal;
+                });
+
+                $scope.openModal = function (content) {
+                    $scope.modalcontent = content;
+                    $scope.modal.show();
+                };
+                $scope.closeModal = function () {
+                    $scope.modal.hide();
                 };
 
             }]);

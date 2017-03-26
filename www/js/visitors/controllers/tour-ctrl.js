491eba06a63d33cc0b6727ae59b6f2653211453b
gerald Chablowski <g.chablowski@gmail.com>
2017-03-26 16:03:14 +0200

    use ionicpopup for tour form

--- www/js/visitors/controllers/tour-ctrl.js
+++ www/js/visitors/controllers/tour-ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('visitors')
-        .controller('TourCtrl', ["$scope", "datasets", "MainServ", function ($scope, datasets, MainServ) {
+        .controller('TourCtrl', ["$scope", "datasets", "MainServ", "$ionicPopup", function ($scope, datasets, MainServ, $ionicPopup) {
 
                 var $this = this;
                 $scope.appMessage = {};
@@ -8,32 +8,26 @@ angular.module('visitors')
 
                 $scope.contact = datasets.contact;
 
+                $this.showAlert = function (title, template) {
+                    $ionicPopup.alert({
+                        title: title,
+                        template: template
+                    });
+                };
+
                 $this.success = function (data) {
 
                     if (data.error) {
-                        $scope.appMessage.title = 'Invalid Details';
-                        $scope.appMessage.content = data.error;
-                        $scope.appMessage.show = true;
+                        $this.showAlert('Invalid Details', data.error);
                     } else {
-                        $scope.appMessage.title = 'Sent!';
-                        $scope.appMessage.content = 'Thank you for your enquiry, we will get back to you as soon as possible!';
-                        $scope.appMessage.show = true;
+                        $this.showAlert('Sent!', 'Thank you for your enquiry, we will get back to you as soon as possible!');
                     }
                 };
 
                 $scope.sendForm = function () {
 
                     if (!$scope.formData.terms) {
-                        $scope.appMessage.title = 'Terms and Conditions';
-                        $scope.appMessage.content = 'Please accept the terms and conditions';
-                        $scope.appMessage.show = true;
-                        return false;
-                    }
-
-                    if (!$scope.formData.name && !$scope.formData.email && !$scope.formData.message) {
-                        $scope.appMessage.title = 'Invalid Details';
-                        $scope.appMessage.content = 'Please ensure all require fields are filled';
-                        $scope.appMessage.show = true;
+                        $this.showAlert('Terms and Conditions', 'Please accept the terms and conditions');
                         return false;
                     }
 
